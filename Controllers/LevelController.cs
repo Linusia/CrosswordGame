04009e27c87129d50754d0c45efb97f988f15d8c
Karolina <206937397+Linusia@users.noreply.github.com>
2025-12-04 15:28:49 +0100

    Add files via upload

---
+++ Controllers/LevelController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class LevelController : Controller
+{
+    public IActionResult Play(int id)
+    {
+        return Content($"Tu będzie poziom nr {id}");
+    }
+}
---
+++ Controllers/LevelsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class LevelsController : Controller
+{
+    public IActionResult Index(int page = 1)
+    {
+        return View("Levels", page);
+    }
+}
